c20d577423a9c8ed177627e9727444374ed16655
ehuaranga <edgar>
2018-11-03 12:43:00 -0500

    primer commit CTIC IX grupo

---
+++ s1-introduccion/PrimerCommitGithub.sh
@@ -0,0 +1,17 @@
+#Clic derecho desde la ruta C:\BigData\p9-clases-antonio\s1-introduccion y escogemos "Git Bash"
+
+#Dentro de la consola escribir
+
+git init
+git add .
+git commit -m "cambiar por un comentario descriptivo"
+
+#Cambiar la url ... por la suya
+git remote add origin https://github.com/BigDataCTIC/...
+
+
+#Fin
+
+
+
+
