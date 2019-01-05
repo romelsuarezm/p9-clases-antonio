3b79bf12a7d07b6933c8de6aaeb4204a01e9151b
Arturo <arrome10@gmail.com>
2019-01-05 09:13:25 -0500

    [ADD] Util directory

---
+++ util/create-cluster-dataproc.sh
@@ -0,0 +1,4 @@
+import org.apache.spark.sql.functions.{min, max} 
+df.agg(min("open"), max("open"))
+
+gcloud dataproc clusters create cluster-cticName --subnet default --zone asia-northeast1-b --master-machine-type n1-standard-2 --master-boot-disk-size 300 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 200 --optional-components=ANACONDA,JUPYTER --image-version 1.3-deb9 --project demodata-227400
