414469966bb4c163648df3a93542bbcf3f5c0a7c
frankfalconc <44730949+frankfalconc@users.noreply.github.com>
2019-01-06 10:21:01 -0500

    Update create-cluster-dataproc.sh

--- util/create-cluster-dataproc.sh
+++ util/create-cluster-dataproc.sh
@@ -1,2 +1,2 @@
-#Cambiar el valor ctic-227716
+#Cambiar el valor ctic-227716 - Conetividad con juPYTER
 gcloud beta dataproc clusters create cluster-ctic --subnet default --zone us-west1-a --master-machine-type n1-standard-2 --master-boot-disk-size 300 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 200 --optional-components=ANACONDA,JUPYTER --image-version 1.3-deb9 --project ctic-227716
